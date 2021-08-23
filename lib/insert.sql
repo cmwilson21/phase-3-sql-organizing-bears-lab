c4d5e902faa6ed444a3ae5c26c6254b2c12f25a8
Courtney Wilson <cmwilson132@gmail.com>
2021-08-22 22:27:11 -0500

    commit tree 4402014462af3b9746f0dfc769cf2dd34b0351ce

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Jake", 4, "M", "brown", "happy", 1);
+INSERT INTO bears VALUES (2, "Joanie", 8, "F", "grey", "sleepy", 1);
+INSERT INTO bears VALUES (3, "Wild Cub", 41, "M", "black", "stern", 0);
+INSERT INTO bears VALUES (4, "Frank", 1, "M", "brown", "happy", 1);
+INSERT INTO bears VALUES (5, "Goldie", 20, "F", "white", "happy", 1);
+INSERT INTO bears VALUES (6, "Sundae", 4, "F", "brown", "happy", 1);
+INSERT INTO bears VALUES (7, "Bobby", 14, "M", "brown", "happy", 1);
+INSERT INTO bears VALUES (8, null, 9, "M", "brown", "happy", 1);
